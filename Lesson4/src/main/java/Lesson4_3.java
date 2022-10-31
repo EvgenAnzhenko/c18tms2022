11022754ebe97f7829c21bccd7573039b0c721e0
EvgenAnzhenko <forby@mail.ru>
2022-10-31 11:42:09 +0300

    Lesson 4 Fix1

--- Lesson4/src/main/java/Lesson4_3.java
+++ Lesson4/src/main/java/Lesson4_3.java
@@ -1,27 +1,20 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lesson4_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Double> list = new ArrayList<>();
-        int i = 0;
+        System.out.println("1 число");
+        double a = scanner.nextDouble();
+        System.out.println("2 число");
+        double b = scanner.nextDouble();
+        System.out.println("3 число");
+        double c = scanner.nextDouble();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        c = Math.abs(c);
+        double minAB = Math.min(a, b);
+        System.out.println(Math.min(minAB, c));
 
-        while (i != 3) {
-            System.out.println("Вводим " + (i + 1) + " число");
-            list.add(i, scanner.nextDouble());
-            i++;
-        }
-        double min = Math.abs(list.get(0));
-        double result_min = 0;
-        for (int n = 0; n < list.size(); n++) {
-            if (Math.abs(list.get(n)) < min) {
-                min = Math.abs(list.get(n));
-                result_min = list.get(n);
-            }
-
-        }
-        System.out.println(result_min);
     }
 
 }
