af144783302de7f8db02d90172d970d5c6f517e8
EvgenAnzhenko <forby@mail.ru>
2022-11-01 01:02:27 +0300

    Lesson 4 Fix3

--- Lesson4/src/main/java/Lesson4_3.java
+++ Lesson4/src/main/java/Lesson4_3.java
@@ -1,21 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lesson4_3 {
     public static void main(String[] args) {
+        System.out.println("Вводим количество чисел");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1 число");
-        double a = scanner.nextDouble();
-        System.out.println("2 число");
-        double b = scanner.nextDouble();
-        System.out.println("3 число");
-        double c = scanner.nextDouble();
-        a = Math.abs(a);
-        b = Math.abs(b);
-        c = Math.abs(c);
-        double minAB = Math.min(a, b);
-        System.out.println(Math.min(minAB, c));
-
+        if (scanner.hasNextInt()) {
+            int amount = scanner.nextInt();
+            addList(amount);
+        }
     }
 
+    public static void addList(int amount) {    // метод для заполнения массива
+        ArrayList<Double> list = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Вводим " + (i + 1) + " число");
+            Scanner scanner = new Scanner(System.in);
+            list.add(scanner.nextDouble());
+        }
+        minAbs(list);
+    }
+
+    public static void minAbs(ArrayList<Double> list) {  //метод для нахождения мин модуля
+        double min = Math.abs(list.get(0));
+        for (int n = 0; n < list.size(); n++) {
+            if (Math.abs(list.get(n)) < min) {
+                min = Math.abs(list.get(n));
+            }
+        }
+        System.out.println(min);
+    }
 }
 
