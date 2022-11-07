a2e7ec0a836c701fe2621736edd4d5de354bbd2b
EvgenAnzhenko <70512765+EvgenAnzhenko@users.noreply.github.com>
2022-11-08 00:29:37 +0300

    Update Lesson5Task2.java

--- Lesson5/src/main/java/Lesson5Task2.java
+++ Lesson5/src/main/java/Lesson5Task2.java
@@ -5,11 +5,10 @@ public class Lesson5Task2 {
         //2) Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 //         сколько амеб будет через 3, 6, 9, 12,..., 24 часа
 
-        int ameba = 1;
         System.out.println("Введите количество часов (кратные 3)");
         Scanner scanner = new Scanner(System.in);
         int hours = scanner.nextInt() / 3;
-        int ValueAmeba = ameba * (int) Math.pow(2, hours);
+        int ValueAmeba = (int) Math.pow(2, hours);
         System.out.println("Количество амеб будет " + ValueAmeba);
     }
 }
