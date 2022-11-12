9e156b991e5a251e0fbbb44bb8671631b3a25557
EvgenAnzhenko <forby@mail.ru>
2022-11-12 17:14:42 +0300

    LessonFullFix_1

--- Lesson5/src/main/java/Lesson5Task13.java
+++ Lesson5/src/main/java/Lesson5Task13.java
@@ -55,7 +55,7 @@ public class Lesson5Task13 {
         String result;
         for (int i = 0; i < mass.length; i++) {   //Вывод новой матрицыматрицы
             for (int j = 0; j < mass[i].length; j++) {
-                if (((mass[i][j] % 3 == 0) && (mass[i][j] % 7 == 0)) | (mass[i][j] % 3 == 0)) {
+                if (mass[i][j] % 3 == 0) {
                     result = "+";
                 } else if (mass[i][j] % 7 == 0) {
                     result = "-";
