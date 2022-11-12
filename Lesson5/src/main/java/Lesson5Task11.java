241537eec5139977e668105fe89f79b08bb64828
EvgenAnzhenko <forby@mail.ru>
2022-11-12 12:40:20 +0300

    LessonFullFix

--- Lesson5/src/main/java/Lesson5Task11.java
+++ Lesson5/src/main/java/Lesson5Task11.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class Lesson5Task11 {
     public static void main(String[] args) {
 //      11) Проверить, различны ли все элементы массива, если не различны то вывести элемент повторяющийся
-//        Пример: {0,3,46,3,2,3,2}
+//        Пример: {0,3,46,3,2,1,2}
 //        Массив имеет повторяющиеся элементы 3, 2
 //        Пример: {0,34,46,31,20,1,28}
 //        Массив не имеет повторяющихся элементов
-        int[] mass = {0, 34, 46, 31, 20, 1, 28};
+        int[] mass = {0, 3, 46, 3, 2, 1, 2};
         ArrayList<Integer> list = new ArrayList<>();
         for (int count = 0; count < mass.length; count++) {
             for (int countNew = count + 1; countNew < mass.length; countNew++) {
@@ -17,7 +17,15 @@ public class Lesson5Task11 {
             }
         }
         if (list.size() > 0) {
-            System.out.print("Массив имеет повторяющиеся элементы " + list);
+            System.out.print("Массив имеет повторяющиеся элементы ");
+            int result = 0;
+            for (int i = 0; i < list.size(); i++) {
+                result = list.get(i);
+                System.out.print(result);
+                if (list.get(i) != list.size()) {
+                    System.out.print(",");
+                }
+            }
         } else {
             System.out.print("Массив не имеет повторяющихся элементов");
         }
