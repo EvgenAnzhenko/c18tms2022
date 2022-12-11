d73ac1059d57360809f43ddd14d6c5d4fc933535
EvgenAnzhenko <forby@mail.ru>
2022-12-12 00:48:27 +0300

    Lesson11Fruits

--- Lesson11/src/main/java/ru/fruit/model/Pear.java
+++ Lesson11/src/main/java/ru/fruit/model/Pear.java
@@ -1,17 +1,12 @@
 package ru.fruit.model;
 
 public class Pear extends Fruit {
-    private static double fullWeight;
-    public static double fullPearCost;
-
     public Pear(double weight, double pricePerKilogram) {
         super(weight, pricePerKilogram);
-        fullWeight += weight;
-        fullPearCost += weight * pricePerKilogram;
     }
 
     @Override
     public double getCostFruit() {
-        return fullWeight * getPricePerKilogram();
+        return getWeight() * getPricePerKilogram();
     }
 }
