173e920ea8832d407fa594bea873a3cf6dada212
EvgenAnzhenko <forby@mail.ru>
2022-12-14 22:57:34 +0300

    Lesson12

--- Lesson12/src/main/java/ru/autorization/service/Main.java
+++ Lesson12/src/main/java/ru/autorization/service/Main.java
@@ -6,18 +6,19 @@ import ru.autorization.exception.WrongPasswordException;
 public class Main {
     public static void main(String[] args) {
         try {
-            checkAuthorization("sdssdsddfd3", "5456461", "545646");
+            checkAuthorization("sdssdsddfd3", "545646", "545646");
         } catch (WrongPasswordException | WrongLoginException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    static void checkAuthorization(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+    static boolean checkAuthorization(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
         if (!checkLength(login) || !login.matches(("^[a-zA-Z0-9_]+$"))) {
             throw new WrongLoginException("Ошибка ввода Логина");
         } else if (!checkLength(password) || !login.matches(("^[a-zA-Z0-9_]+$")) || !password.equals(confirmPassword)) {
             throw new WrongPasswordException("Ошибка ввода пароля");
         }
+        return true;
     }
 
     static private boolean checkLength(String parameter) {
